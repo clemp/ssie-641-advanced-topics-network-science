33fa16ecca841785dffaa49afc79d57a66a00a65
Christian Lemp <christianlemp@gmail.com>
2022-10-26 12:52:58 -0600

    random seed and charts

--- notebooks/Network_dynamics_HW2_1.py
+++ notebooks/Network_dynamics_HW2_1.py
@@ -4,6 +4,11 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
+SEED = 315 
+
+# Set randomness
+np.random.seed(315)
+
 # Set variables
 STEPS = 100
 
@@ -21,7 +26,7 @@ if __name__ == "__main__":
     ITER_NODES = [10, 100, 1000, 10000]
     fig, axs = plt.subplots(NUM_ACROSS, NUM_DOWN, sharex=False, sharey=True)
     fig.tight_layout(pad=5.0)
-    fig.suptitle('Node degree distribution of a random network in semi-log scale')
+    fig.suptitle('Node degree distribution $k$ of a random network in semi-log scale')
 
     # iterate over discrete number of steps and generate the netowrk
     for iter in ITER_NODES:
@@ -34,9 +39,6 @@ if __name__ == "__main__":
             else:
                 pass
 
-        # nx.draw_circular(G, node_size=4)
-        # plt.show()
-
         # Analyze the randomly generated network
         degrees = [n[1] for n in G.degree]
         degree_values = list(set(degrees))
@@ -44,8 +46,11 @@ if __name__ == "__main__":
 
         k_lst = list(Pk.keys())
         Pk_lst = list(Pk.values())
-        Pk_log_lst = [np.log(pk) for pk in Pk_lst]
+        Pk_log_lst = [np.log10(pk) for pk in Pk_lst]
 
+        print("Max node degree for graph with ", str(iter), " nodes: \t", max(k_lst))
+        print("Min node degree for graph with ", str(iter), " nodes: \t", min(k_lst))
+        
         # Save results to data structure
         all_k.append(k_lst)
         all_Pk.append(Pk_log_lst)
@@ -55,11 +60,22 @@ if __name__ == "__main__":
     plt.rc("text", usetex=False)
     plt.rc("font", family='serif')
 
+    # Iterate through each generated network data from previous step
     for pi in range(NUM_ACROSS):
         for pj in range(NUM_DOWN):
+            # Map matrix [i,j] values to index in a flat array
             idx = pi * NUM_ACROSS + pj
-            axs[pi % NUM_ACROSS, pj % NUM_DOWN].plot(all_k[idx], all_Pk[idx])
+
+            # Calculate best fit line for each k, P(k) generated data
+            a, b = np.polyfit(all_k[idx], all_Pk[idx], 1)
+
+            axs[pi % NUM_ACROSS, pj % NUM_DOWN].plot(all_k[idx], all_Pk[idx]) # k, P(k)
+            axs[pi % NUM_ACROSS, pj % NUM_DOWN].plot(all_k[idx], np.multiply(a, all_k[idx]) + b, linestyle="--", linewidth=1) # best fit line
             axs[pi % NUM_ACROSS, pj % NUM_DOWN].set_title(str(ITER_NODES[idx]) + ' Nodes')
+
+            #add fitted regression equation to plot
+            # axs[pi % NUM_ACROSS, pj % NUM_DOWN].text(1, 17, 'y = ' + '{:.2f}'.format(b) + ' + {:.2f}'.format(b) + 'x', size=8)
+            print("Slope of best fit line: \t", a)
     
     # axs[0, 0].plot(k_lst, Pk_log_lst)
     # axs[0, 0].set_title('10 Nodes')
@@ -72,7 +88,7 @@ if __name__ == "__main__":
 
     # After all subplot iterations, label the plot
     for ax in axs.flat:
-        ax.set(xlabel='k', ylabel='ln(P(k))')
+        ax.set(xlabel='k', ylabel='$log(P(k))$')
 
     # Hide x labels and tick labels for top plots and y ticks for right plots.
     # for ax in axs.flat:
@@ -81,13 +97,8 @@ if __name__ == "__main__":
     plt.savefig("../output/images/random-network_node_degree_analysis.png")
     plt.show()
 
-    # plt.plot(k_lst, Pk_log_lst)
-    # line1 = plt.plot([0,100],[100,0], c='red', alpha=0.15, lw=1)
-    # line2 = plt.plot([0,1,4],[3, 2,4],  c='green', lw=1)
-    # # text is left-aligned
-    # plt.text(50,50,'This text starts at point (2,4)')
-    # plt.show()
+    # Draw the final network graph
+    nx.draw_circular(G, node_size=4)
+    plt.savefig("../output/images/random-network_network_viz.png")
+    plt.show()
 
-        # print(G)
-       
-       # Randomly add edge
